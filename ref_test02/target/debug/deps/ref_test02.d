7880b71ced0507c5b1dc7eae5149dfdaee49fef0
Sung-Hyuk Lee <merci726@yahoo.com>
2023-12-20 13:08:24 +0900

    12.20.2023 update

---
+++ ref_test02/target/debug/deps/ref_test02.d
@@ -0,0 +1,5 @@
+C:\MyProject\Rust_Practice\ref_test02\target\debug\deps\ref_test02.exe: src\main.rs
+
+C:\MyProject\Rust_Practice\ref_test02\target\debug\deps\ref_test02.d: src\main.rs
+
+src\main.rs:
---
+++ ref_test02/target/debug/ref_test02.d
@@ -0,0 +1,1 @@
+C:\MyProject\Rust_Practice\ref_test02\target\debug\ref_test02.exe: C:\MyProject\Rust_Practice\ref_test02\src\main.rs
