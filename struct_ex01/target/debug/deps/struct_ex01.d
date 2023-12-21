8f9541e6be8c24d7873893c38337d1523bd41fd0
Sung-Hyuk Lee <merci726@yahoo.com>
2023-12-21 22:13:02 +0900

    12.21.2023 update

---
+++ struct_ex01/target/debug/deps/struct_ex01.d
@@ -0,0 +1,5 @@
+C:\MyProject\Rust_Practice\struct_ex01\target\debug\deps\struct_ex01.exe: src\main.rs
+
+C:\MyProject\Rust_Practice\struct_ex01\target\debug\deps\struct_ex01.d: src\main.rs
+
+src\main.rs:
---
+++ struct_ex01/target/debug/struct_ex01.d
@@ -0,0 +1,1 @@
+C:\MyProject\Rust_Practice\struct_ex01\target\debug\struct_ex01.exe: C:\MyProject\Rust_Practice\struct_ex01\src\main.rs
