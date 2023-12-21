900d093065ec55277a58027bc8287c8a0386f869
Sung-Hyuk Lee <merci726@yahoo.com>
2023-12-21 22:10:33 +0900

    12.21.2023 update

--- struct_test01/src/main.rs
+++ struct_test01/src/main.rs
@@ -16,4 +16,18 @@ fn main() {
     println!("active: {}", if user1.active{"활성"}else{"비활성"});
     println!("username: {}", user1.username);
     println!("sic: {}", user1.sign_in_count);
+    let user2 = build_user("proleesh@rust.me".to_string(), "proleesh".to_string());
+
+    println!("email: {}", user2.email);
+    println!("username: {}", user2.username);
+    println!("active: {}", if user2.active{"활성"}else{"비활성"});
+    println!("sic: {}", user2.sign_in_count);
+}
+fn build_user(email: String, username: String) -> User{
+    User{
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
