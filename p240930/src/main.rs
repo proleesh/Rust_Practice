bd6bc86fdc57a857bb3c0a6e36631851b39817b9
proleesh <merci726@yahoo.com>
2024-09-30 23:37:24 +0900

    update

--- p240930/src/main.rs
+++ p240930/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x:i32 = 3;
+    let x = x + 2;
+    let x = x * 2;
+    println!("x: {}", x);
+
+    let x = "Hello, Rust!";
+    println!("x: {}", x);
 }
