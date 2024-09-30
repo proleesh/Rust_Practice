1243707a093d508632510fea6e5389b113a89b61
proleesh <merci726@yahoo.com>
2024-09-30 23:42:08 +0900

    update

--- p240930/src/main.rs
+++ p240930/src/main.rs
@@ -6,4 +6,19 @@ fn main() {
 
     let x = "Hello, Rust!";
     println!("x: {}", x);
+
+    let integer1: u32 = 17;
+    let integer2 = 17u32;
+    let integer3 = 17;
+    let integer4: u32 = 0b10001;
+    let integer5: u32 = 0o21;
+    let integer6: u32 = 0x11;
+    let integer7 = 50_000;
+    println!("integer1: {}", integer1);
+    println!("integer2: {}", integer2);
+    println!("integer3: {}", integer3);
+    println!("integer4: {}", integer4);
+    println!("integer5: {}", integer5);
+    println!("integer6: {}", integer6);
+    println!("integer7: {}", integer7);
 }
