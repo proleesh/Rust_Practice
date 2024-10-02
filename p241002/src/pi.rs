8978abc84c5b4362472460b563ee5646fb6c7a3a
proleesh <merci726@yahoo.com>
2024-10-02 09:10:17 +0900

    update 10.2

--- p241002/src/pi.rs
+++ p241002/src/pi.rs
@@ -1,7 +1,12 @@
+fn calc_circle_area(_radius: f32) -> f32 {
+    3.14 * 4.0 * 4.0
+}
 pub fn pi(){
     // let pi = 3.14;
     let pi = std::f64::consts::PI;
 
     let area = pi * 4.0 * 4.0;
     print!("area: {} ", area);
+    println!();
+    print!("area: {} ", calc_circle_area(10.0));
 }
