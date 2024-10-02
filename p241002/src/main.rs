821ce5d490b49b748d4cb926db083a74ed2d5008
proleesh <merci726@yahoo.com>
2024-10-02 09:01:03 +0900

    update 10.2

---
+++ p241002/src/main.rs
@@ -0,0 +1,14 @@
+mod pi;
+
+fn main() {
+    let array = [1,2,3];
+/*    for i in 0..=2 {
+        print!("{} ", array[i]);
+    }*/
+    for item in & array {
+        print!("{} ", item);
+    }
+
+    println!();
+    pi::pi();
+}
---
+++ p241002/src/pi.rs
@@ -0,0 +1,5 @@
+pub fn pi(){
+    let pi = 3.14;
+    let area = pi * 4.0 * 4.0;
+    print!("area: {} ", area);
+}
